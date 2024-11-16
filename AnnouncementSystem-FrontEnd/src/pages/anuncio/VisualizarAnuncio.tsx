cca287d86d660bb25bf391b8c1ba75ad3a0a6318
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-11-16 20:23:10 -0300

    Visualizar anuncio completo

--- AnnouncementSystem-FrontEnd/src/pages/anuncio/VisualizarAnuncio.tsx
+++ AnnouncementSystem-FrontEnd/src/pages/anuncio/VisualizarAnuncio.tsx
@@ -1,14 +1,85 @@
-import {Link, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {Ad, AdSchema} from "../../schema/AdSchema.tsx";
 import api from "../../services/api.tsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStarHalfStroke, faTag} from "@fortawesome/free-solid-svg-icons";
+import {getDownloadURL, listAll, ref} from "firebase/storage";
+import {storage} from "../../services/firebaseConfig.tsx";
 
 function VisualizarAnuncio() {
 
     const { id } = useParams();
     const [ad, setAd] = useState<Ad>();
     const [error, setError] = useState<string>("");
+    const [imgPerfil, setImgPerfil] = useState<string>('/images/img-padrao.PNG');
+    const [images, setImagens] = useState<string[]>([]);
+    const defaultImage = "/images/img-padrao.PNG";
+    const [selectedImage, setSelectedImage] = useState<string>(defaultImage);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
+    const handlePrevious = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
+    const handleNext = () => {
+        if (currentIndex < images.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        }
+    };
+
+    const visibleImages = images.slice(currentIndex, currentIndex + 3);
+
+    const formatDate = (date: string) => {
+        const options: Intl.DateTimeFormatOptions = {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        };
+
+        return new Date(date).toLocaleDateString('pt-BR', options);
+    };
+    const formatScore = (score: number) => {
+        return score.toLocaleString('pt-BR', { minimumFractionDigits: 1, maximumFractionDigits: 1 });
+    };
+
+    const fetchImages = (nomeArquivo: string | null | undefined) => {
+        if(!nomeArquivo){
+            return
+        }
+        const imageListRef = ref(storage, `${nomeArquivo}/`);
+        listAll(imageListRef)
+            .then((response) => {
+                if (response.items.length > 0) {
+                    Promise.all(
+                        response.items.map((item) => getDownloadURL(item))
+                    ).then((urls) => {
+                        setImagens(urls);
+                        setSelectedImage(urls[0])
+                    });
+                } else {
+                    console.log("Nenhuma imagem encontrada.");
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar as imagens:", error);
+            });
+    };
+    const fetchImgPerfil = (img: string | null | undefined) => {
+        if (!img) {
+            return;
+        }
+        const imageRef = ref(storage, img);
+        getDownloadURL(imageRef).then((url) => {
+            setImgPerfil(url);
+        }).catch(error => {
+            console.error("Erro ao buscar a imagem:", error);
+        });
+    };
     async function fetchAd() {
         try {
             const response = await api.get(`/announcement/${id}`);
@@ -26,6 +97,8 @@ function VisualizarAnuncio() {
                 throw new Error('Dados inválidos recebidos');
             }
             setAd(parsedResult.data);
+            fetchImgPerfil(parsedResult.data.author.icon)
+            fetchImages(parsedResult.data.imageArchive)
         } catch (error: unknown) {
             if (error instanceof Error) {
                 setError(error.message);
@@ -42,53 +115,111 @@ function VisualizarAnuncio() {
     if (error) return <p className="text-red-500">Erro: {error}</p>;
     if (!ad) return <p>Carregando...</p>;
 
-    const formatDate = (date: string) => {
-        const options: Intl.DateTimeFormatOptions = {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-        };
-        return new Date(date).toLocaleDateString('pt-BR', options);
-    };
-
     return (
         <main className="main-layout">
-
-            <div className="flex w-full justify-between">
-                <p className="mt-2 text-gray-600">{ad.city.name}</p>
-                <p className="text-sm">
-                    {formatDate(ad.date)}
-                </p>
-            </div>
-
-            {/* Imagens */}
-
-            <div className="flex justify-center mb-3">
-                <h1 className="text-2xl font-semibold text-gray-900">{ad.title}</h1>
-            </div>
-
-            <div className="flex justify-start">
-                <p className="text-gray-700">{ad.content}</p>
-            </div>
-
-            {ad.price != null && ad.price > 0 && (
-                <div className="flex justify-end my-2">
-                    <p className="font-semibold text-gray-800">
-                        R$ {ad.price.toFixed(2)}
-                    </p>
+            <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
+                <div className="grid grid-cols-2 text-sm md:order-2">
+                    <div>{ad.city.name}</div>
+                    <div className="text-end">{formatDate(ad.date)}</div>
                 </div>
-            )}
+                <div className="flex flex-col items-center md:order-1 md:row-span-6">
+                    <div className="mb-4">
+                        <img
+                            src={selectedImage}
+                            alt="Imagem principal"
+                            className="w-96 h-96 object-cover rounded-md"
+                        />
+                    </div>
 
-            <div className="flex justify-between">
-                <p>
-                    {ad.categories.map(category => category.name).join(', ')}
-                </p>
-                <Link to={`/author/${ad.author.email}`}
-                      className="hover:underline">
-                    {ad.author.name}
-                </Link>
+                    <div className="flex items-center space-x-2">
+                        {currentIndex > 0 && (
+                            <button
+                                className="text-gray-700 hover:text-gray-900"
+                                onClick={handlePrevious}
+                            >
+                                {"<"}
+                            </button>
+                        )}
+
+                        <div className="flex space-x-3">
+                            {visibleImages.length > 0
+                                ? visibleImages.map((image, index) => (
+                                    <img
+                                        key={index}
+                                        src={image}
+                                        alt={`Imagem ${index + 1}`}
+                                        className={`w-20 h-20 object-cover rounded-md cursor-pointer ${
+                                            selectedImage === image ? "ring-2 ring-blue-500" : ""
+                                        }`}
+                                        onClick={() => setSelectedImage(image)}
+                                    />
+                                ))
+                                : (
+                                    <img
+                                        src={defaultImage}
+                                        alt="Imagem padrão"
+                                        className="w-20 h-20 object-cover rounded-md cursor-pointer"
+                                        onClick={() => setSelectedImage(defaultImage)}
+                                    />
+                                )}
+                        </div>
+
+                        {currentIndex < images.length - 3 && (
+                            <button
+                                className="text-gray-700 hover:text-gray-900"
+                                onClick={handleNext}
+                            >
+                                {">"}
+                            </button>
+                        )}
+                    </div>
+                </div>
+                <div className="text-2xl font-semibold md:order-2">{ad.title}</div>
+                <div className="md:order-2">
+                    {ad.content}
+                </div>
+                <div className="flex justify-between md:order-2">
+                    <div className="text-lg font-medium content-center">{
+                        ad.price != null && ad.price > 0 &&
+                        (<span>R$ {ad.price.toLocaleString('pt-BR', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2
+                        })}</span>)
+                    }</div>
+                    <div className="flex justify-center align-middle">
+                        <div className="w-16 h-16 justify-center">
+                            <img
+                                src={imgPerfil}
+                                alt="Imagem de Perfil"
+                                className="rounded-full"
+                            />
+                        </div>
+                        <div className="flex flex-col justify-center p-1">
+                            <div className="">{ad.author.name}</div>
+                            <div>
+                                <FontAwesomeIcon icon={faStarHalfStroke} className="me-1 text-yellow-400"/>
+                                {formatScore(ad.author.score)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center md:order-2">
+                    <div className="flex items-center text-xs">
+                        {ad.categories.map((category, index) => (
+                            <div key={index} className="">
+                                <span className="inline-block bg-blue-200 text-blue-800 px-3 py-1 me-1 rounded-full">
+                                  <FontAwesomeIcon icon={faTag}/> {category?.name}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                    <div>
+                        <button className="btn-primary">
+                            Contatar
+                        </button>
+                    </div>
+                </div>
             </div>
-
         </main>
     );
 }
