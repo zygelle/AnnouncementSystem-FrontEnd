701afc18f684ee1b80fb6785bebc0b4078771318
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-01-10 11:29:11 -0300

    Update Paths.tsx

--- AnnouncementSystem-FrontEnd/src/routers/Paths.tsx
+++ AnnouncementSystem-FrontEnd/src/routers/Paths.tsx
@@ -2,16 +2,16 @@ export const pathLogin = '/login'
 export const pathCreateAds = '/anuncio/criar'
 export const pathFilterAd = '/anuncio/buscar'
 export const pathHome = '/'
-export const pathCommunication = '/perfil/comunicacao'
-export const pathPerfil = '/perfil'
-export const pathMeusAnuncios = '/perfil/anuncios'
+export const pathCommunication = '/comunicacao'
+export const pathPerfil = '/meu-perfil'
+export const pathMeusAnuncios = '/meus-anuncios'
 export const pathEditarAnuncio = '/anuncio/editar/:id'
 export const pathVisualizarAnuncio = `/anuncio/:id`
 export const setPathVisualizarAnuncio = (id: string) => {
     return `/anuncio/${id}`
 }
-export const pathVizualizarAnunciante = '/anunciante/:name'
+export const pathVizualizarAnunciante = '/perfil/:name'
 export const setPathVizualizarAnunciante = (name: string) => {
-    return `/anunciante/${name}`
+    return `/perfil/${name}`
 }
 export const pathAssessment = '/avaliacao'
